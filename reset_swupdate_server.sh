bea5fa19285ce3abec54d368a3f4e4cb89ba8a6b
cvwong <macwongcv@icloud.com>
2021-04-16 18:57:16 +0800

    Delete reset_swupdate_server.sh

--- reset_swupdate_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-/Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin stop swupdate
-rm -rf /Library/Server/Software\ Update/
-/Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin settings swupdate:portToUse=8088
-/Applications/Server.app/Contents/ServerRoot/usr/sbin/serveradmin start swupdate
-exit 0
