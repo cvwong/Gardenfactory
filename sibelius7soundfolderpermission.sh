e1223b44e7f8d87d254fd884786425eb92085135
cvwong <macwongcv@icloud.com>
2021-04-16 18:56:52 +0800

    Delete sibelius7soundfolderpermission.sh

--- sibelius7soundfolderpermission.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Change the ownership of the files and folders
-sudo chown -R gis:admin /Library/Application\ Support/Avid/Sibelius\ Sounds/
-
-#Change the permission of the files and folders
-sudo chmod -R 775 /Library/Application\ Support/Avid/Sibelius\ Sounds/
-
-exit 0
